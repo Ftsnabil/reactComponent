8b643eef5a4ace38a44ff0bedb3787d7e513f9ad
Ftsnabil <nabfts116@gmail.com>
2021-09-01 22:27:53 +0100

    first commit

---
+++ project/src/Component/Profile/Address.js
@@ -0,0 +1,5 @@
+import '../../index.css';
+function Address(){
+    return <h1> Baten-Kairouan 3140</h1>
+}
+export default Address
---
+++ project/src/Component/Profile/FullName.js
@@ -0,0 +1,5 @@
+import '../../index.css';
+function FullName(){
+    return <h1> Fatnassi Nabil </h1>
+}
+export default FullName
